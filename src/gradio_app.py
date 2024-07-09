3e25f80d01aaa44af2044f467c6c96b5254a28bb
ColombIATeam <163057153+ColombIATeam@users.noreply.github.com>
2024-07-09 14:44:37 -0500

    Add files via upload

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,10 +1,18 @@
+from fastapi import FastAPI
+import gradio as gr
+
+app = FastAPI()
+
 from server.workflow_utils import Work_Flow
 work_flow_utils = Work_Flow()
-
-from server.events import Events 
+ 
+from server.events import Events#
 linked_events =Events(work_flow_utils)
-
+ 
 from server.gradio_UI import UI
 interfas = UI(work_flow_utils, linked_events)
+ 
 
-interfas.run_UI()
+app = gr.mount_gradio_app(app, interfas.run_UI(), path="/gradio")
+#interfas.run_UI()
+
