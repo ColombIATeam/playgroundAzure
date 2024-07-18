e126a7423ef1f2f2c8426e3eb87a8dab08ddafeb
ColombIATeam <163057153+ColombIATeam@users.noreply.github.com>
2024-07-18 11:39:38 -0500

    Add files via upload

--- src/common/application_settings.py
+++ src/common/application_settings.py
@@ -8,29 +8,14 @@ from pydantic_settings import (
 from typing import Optional
 
 class ApplicationSettings(BaseSettings):
-    #model_config = SettingsConfigDict(env_nested_delimiter="__", extra="ignore")
     
     GLOBAL__ENVIRONMENT: str
     OPENAI_API_KEY: str
     CLAUDE_API_KEY: Optional[str] = None
-    #PHOENIX_PROJECT_NAME: str
-    #PHOENIX_COLLECTOR_ENDPOINT: Optional[str] = None
-    #PHOENIX_PORT: Optional[int] = 6006
     OPENAI_ORG_ID: str
     AZURE_OPENAI_ENDPOINT: str
     AZURE_OPENAI_API_KEY: str
     OPENAI_API_VERSION: str
-    
-    # OPEN_AI_EMBEDDINGS__API_TYPE: str
-    # OPEN_AI_EMBEDDINGS__API_KEY: str
-    # OPEN_AI_EMBEDDINGS__AZURE_ENDPOINT: str
-    # OPEN_AI_EMBEDDINGS__API_VERSION: str
-
-    # QDRANT__ENDPOINT: str
-    # QDRANT__API_KEY: str
-    # QDRANT__PORT: int
-
-    # APPLICATION_INSIGHTS__CONNECTION_STRING: str
  
     SQL_SERVER__HOST: str 
     SQL_SERVER__PORT: int 
@@ -38,14 +23,7 @@ class ApplicationSettings(BaseSettings):
     SQL_SERVER__PASSWORD: str 
     SQL_SERVER__NAME: str 
     
-    # SQL_SERVER__CONNECTION_STRING: str
-    # SQL_SERVER__HOST: str = "mssql-server-preparador-dev-we-001.database.windows.net"
-    # SQL_SERVER__PORT: int = 1433
-    # SQL_SERVER__USERNAME: str = "user4defOP7r479X"
-    # SQL_SERVER__PASSWORD: str = "v3lllscr37-244saafD-wd33434DLz"
-    # SQL_SERVER__NAME: str = "preparador"
-    
     class Config:
-        env_file = [".env", f".env.{os.environ['GLOBAL__ENVIRONMENT']}"]
+        env_file = os.getenv('GLOBAL__ENVIRONMENT')
         env_file_encoding = "utf-8"
         extra="ignore"
