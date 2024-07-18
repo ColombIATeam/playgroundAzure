200daa1aadf61e8f450025a139e66a606e7b7f36
ColombIATeam <163057153+ColombIATeam@users.noreply.github.com>
2024-07-18 08:44:21 -0500

    Update gradio_app.py

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -14,8 +14,3 @@ interfas = UI(work_flow_utils, linked_events)
  
 gr_app = interfas.run_UI()
 app = gr.mount_gradio_app(app, gr_app, path="/gradio")
-#interfas.run_UI()
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
